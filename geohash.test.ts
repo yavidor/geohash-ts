d86dacc9541106861773135173ba5df6f6cea94a
yavidor <yonatan@avidor.org>
2025-06-02 18:10:52 +0300

    Added encode test

--- geohash.test.ts
+++ geohash.test.ts
@@ -1,4 +1,5 @@
-import { decode } from "./geohash";
+import { decode, encode } from "./geohash";
 test('9vc0de0nx', () => {
-  expect(decode('9vc0de0nx')).toBeDefined()
+  expect(decode('9vc0de0nx')).toEqual([-99.73356485366821, 32.449257373809814])
+  expect(encode([-99.73356485366821, 32.449257373809814], 9)).toEqual('9vc0de0nx')
 })
